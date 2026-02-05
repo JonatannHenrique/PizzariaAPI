65e7d67df52ba6a308ffb84c6052fdc830966ee1
SeuNome <luana.dabella2006@gmail.com>
2026-02-05 11:22:31 -0300

    .

---
+++ Pizzaria/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace Pizzaria.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int PizzaId { get; set; }
+        public Pizza? Pizza { get; set; } 
+        public int Quantidade { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime Data { get; set; } = DateTime.Now;
+        public string Endereco { get; set; }
+        public string Status { get; set; } = "Pendente";
+    }
+}
---
+++ Pizzaria/Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace Pizzaria.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
