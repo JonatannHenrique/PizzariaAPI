ecdd511f5e4d868e0fd5635161759742b7fbf244
Henrique <luana.dabella2006@gmail.com>
2026-02-05 11:04:06 -0300

    Delete Pizzaria/Models directory

--- Pizzaria/Models/Pizza.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pizzaria.Models;
-public class Pizza
-{
-
-    public int Id { get; set; }
-    public string Nome { get; set; } = string.Empty;
-    public decimal Preco { get; set; }
-
-}
