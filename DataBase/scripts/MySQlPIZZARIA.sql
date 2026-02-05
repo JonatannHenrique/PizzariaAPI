79be44d57d48d2ceb63f14c2b6fe13c12968d471
SeuNome <luana.dabella2006@gmail.com>
2026-02-05 11:49:44 -0300

    DataBaseMySql

---
+++ DataBase/scripts/MySQlPIZZARIA.sql
@@ -0,0 +1,17 @@
+create database Pzz;
+use Pzz;
+
+select * from clientes;
+select * from pizzas;
+select * from pedido;
+INSERT INTO Pizzas (Nome, Preco) VALUES
+('Calabresa', 35.00),
+('Mussarela', 32.00),
+('Frango com Catupiry', 38.00),
+('Portuguesa', 40.00),
+('Quatro Queijos', 42.00),
+('Pepperoni', 45.00),
+('Bacon', 44.00),
+('Marguerita', 36.00),
+('Atum', 39.00),
+('Napolitana', 34.00);
